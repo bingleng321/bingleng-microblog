efdb47fbd0e39f6ec2016921a500d21cc4d31e48
zhuming <15255189959@163.com>
2015-02-09 07:25:20 +0800

    use template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 
-from flask import render_template
+from flask import render_template,flash,redirect
 from app import app
+from forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -20,4 +22,9 @@ def index():
 			}
 		]
 	return render_template('index.html',title='Home',user=user,posts = posts)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html',title='Sigin In',form = form)
 
