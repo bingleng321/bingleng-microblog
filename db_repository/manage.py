9abc3c3a50aff823d2794655742fcfa095f16116
zhuming <15255189959@163.com>
2015-02-10 01:44:02 +0800

    exsercise database

---
+++ db_repository/manage.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from migrate.versioning.shell import main
+
+if __name__ == '__main__':
+    main(six='<module 'six' from '/home/zhuming/develop/git-2.3.0/bingleng-microblog/flask/lib/python2.6/site-packages/six.pyc'>')
