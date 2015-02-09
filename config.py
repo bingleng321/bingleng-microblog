97bd89fb84e79a48b5ccd2978b95d57acb292f52
zhuming <15255189959@163.com>
2015-02-09 19:35:49 +0800

    web forms finished

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+OPENID_PROVIDERS = [
+		{'name':'Google','url':'https://www.google.com/accouts/o8/id'},
+		{'name':'Yahoo','url':'https://me.yahoo.com'},
+		{'name':'AOL','usl':'http:openid.aol.com/<username>'},
+		{'name':'Flickr','url':'http://www.flickr.com/<username>'},
+		{'name':'MyOpenID','url':'https://www.myopenid.com'}
+		]
